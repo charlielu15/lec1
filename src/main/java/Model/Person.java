2a0c2e8c555058ab4df5eaebc5c9370717b36363
charlielu15 <charlie.lu@angra.gmail.com>
2021-10-04 20:58:01 -0400

    second commit for local git Charlie Lu

--- src/main/java/Model/Person.java
+++ src/main/java/Model/Person.java
@@ -38,4 +38,17 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && firstName.equals(person.firstName) && lastName.equals(person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+    }
 }
--- src/main/java/Service/myService1.java
+++ src/main/java/Service/myService1.java
@@ -1,28 +1,34 @@
 package Service;
 
-import Model.Person;
-import org.apache.commons.lang3.StringUtils;
+
+//import org.apache.commons.lang3.StringUtils;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Objects;
 
 public class myService1 {
 
     public static void main(String[] args){
 
-        int int1 = 100;
-        int int2 = int1;
-        int1 = 500;
-        System.out.println("int1: " + int1);
-        System.out.println("int2: " + int2);
+//        int int1 = 100;
+//        int int2 = int1;
+//        int1 = 500;
+//        System.out.println("int1: " + int1);
+//        System.out.println("int2: " + int2);
+//
+//        Integer int3 = 100;
+//        Integer int4 = int3;
+//        int3 = 500;
+//        System.out.println("int3: " + int3);
+//        System.out.println("int4: " + int4);
 
 
-        int[] arr1 = new int[]{1,2,3,4,5};
-
-        int[] arr2 = arr1;
-        arr1[3] = 8;
-        System.out.println("arr1[3]: " + arr1[3]);
-        System.out.println("arr2[3]: " + arr2[3]);
+//        final int[] arr1 = new int[]{1,2,3,4,5};
+//        int[] arr2 = arr1;
+//        arr1[3] = 8;
+//        System.out.println("arr1[3]: " + arr1[3]);
+//        System.out.println("arr2[3]: " + arr2[3]);
 
 //        Person per1 = new Person("Tom", "Cruise", 58);
 //        Person per2 = per1;
@@ -56,7 +62,7 @@ public class myService1 {
 //        Integer i3 = 1000;
 //        Integer i4 = 1000;
 //        System.out.println(i3 == i4);
-
+//
 //        Integer i7 = new Integer(100);
 //        Integer i8 = new Integer(100);
 //        System.out.println(i7 == i8);
@@ -64,7 +70,7 @@ public class myService1 {
 //        String s1 = "100"; // Constant String Pool
 //        String s2 = "100";
 //        System.out.println(s1 == s2);
-
+//
 //        String s3 = new String("100");
 //        String s4 = new String("100");
 //        System.out.println(s3 == s4);
@@ -101,9 +107,62 @@ public class myService1 {
 //                  Deque
 //
 //          Map - LinkedHashMap
-
-
-
-
     }
 }
+
+class Person {
+    String firstName;
+    String lastName;
+    int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Person person = (Person) o;
+//        return age == person.age && firstName.equals(person.firstName) && lastName.equals(person.lastName);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(firstName, lastName, age);
+//    }
+}
+//Hashset of Integers
+//{1, 2, 3, 4} -> 5 -> hahscode(5) hashvalue5 that will generate hash value -> compare to hashcode(1) hashvalue1
+//                                                                                        hashcode(2) hashvalue2
+//                                                                                        hashcode(3) hashvalue3
+//   key is the value for the set elemtn value will be null                               hashcode(4) hashvalue4
+
+// hashvalue5 is equal to hashvalue3 ----> hashcollision
+
